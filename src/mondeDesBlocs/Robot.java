84dce8046dca7ba62ac5beb8c9a1b78a9aa89813
lena <lena.bechet73@gmail.com>
2026-02-22 22:27:51 +0100

    Robot + Table

--- src/mondeDesBlocs/Robot.java
+++ src/mondeDesBlocs/Robot.java
@@ -24,28 +24,107 @@ public class Robot {
         // Automatically generated method. Please do not modify this code.
         this.table = value;
     }
+    
+    private String validerCouleur(String couleur) {
+    	String nouvCouleur;
+    	
+    	if (couleur != null) {
+    		nouvCouleur = couleur;
+    	} else {
+    		nouvCouleur = "rouge";
+    	}
+    	return nouvCouleur;
+    }
+    
+    private Taille validerTaille(Taille taille) {
+    	Taille nouvTaille;
+    	
+    	if (taille != null) {
+    		nouvTaille = taille;
+    	} else {
+    		nouvTaille = Taille.Grand;
+    	}
+    	return nouvTaille;
+    }
 
+    // Permet de créer un cube
     public void creerCube(final String couleur, final Taille taille) {
+    	if (!tientCube()) {
+        	String couleurFinale = validerCouleur(couleur);
+        	Taille tailleFinale = validerTaille(taille);
+        	
+    		Cube nouveau = new Cube(couleurFinale, tailleFinale);
+    		setCubeTenu(nouveau);
+    	} else {
+    		System.out.println("Le robot tient déjà un cube !");
+    	}
     }
 
+    // Permet de verifier si le robot tient un cube
     private boolean tientCube() {
-        // TODO Auto-generated return
-        return false;
+        return getCubeTenu() != null;
     }
 
+    // Saisis un cube
     public void saisirCube(final Cube cube) {
+    	setCubeTenu(cube);
     }
 
+    // Supprime le cube tenu
     public void supprimerCube() {
+    	if (tientCube()) {
+    		saisirCube(null);
+    	} else {
+    		System.out.println("Vous ne tenez pas de cube, nous ne pouvons pas le supprimer !");
+    	}
     }
 
+    // Prend un cube sur la table
     public void prendreCube(final String couleur, final Taille taille) {
+    	if (!tientCube()) {
+        	Table table = this.getTable();
+        	String couleurFinale = validerCouleur(couleur);
+        	Taille tailleFinale = validerTaille(taille);
+        	
+    		Cube nouvCube = table.getCube(couleurFinale, tailleFinale);
+    		
+    		if (nouvCube != null) {
+    			saisirCube(nouvCube);
+    		} else {
+    			System.out.println("Aucun cube n'a été trouvé !");
+    		}
+    	} else {
+    		System.out.println("Vous tenez déjà un cube !");
+    	}
     }
 
+    // Pose le cube tenu sur la table
     public void poserCubeSurTable() {
+    	if (tientCube()) {
+    		Cube cube = this.getCubeTenu();
+    		Table table = getTable();
+    		
+    		table.poserSurTable(cube);
+    		saisirCube(null);
+    	} else {
+    		System.out.println("Vous ne pouvez pas poser un cube inexistant !");
+    	}
     }
 
+    // Pose le cube tenu sur un autre cube
     public void poserCubeSurCube(final String couleur, final Taille taille) {
+    	if (tientCube()) {
+    		Table table = this.getTable();
+        	String couleurFinale = validerCouleur(couleur);
+        	Taille tailleFinale = validerTaille(taille);
+        	
+        	Cube cubeTenu = getCubeTenu();
+        	table.poserCubeSurCube(cubeTenu, couleurFinale, tailleFinale);
+        	
+        	saisirCube(null);
+    	} else {
+    		System.out.println("Vous ne pouvez pas poser un cube inexistant !");
+    	}
     }
 
 }
--- src/mondeDesBlocs/Table.java
+++ src/mondeDesBlocs/Table.java
@@ -16,34 +16,71 @@ public class Table {
         this.cubes = value;
     }
 
+    // Récupère le cube de taille et de couleur données
     public Cube getCube(final String couleur, final Taille taille) {
-        // TODO Auto-generated return
+    	Cube trouve = contientCube(couleur, taille);
+    	
+    	if (trouve != null) {
+    		return depiler(trouve);
+    	}
+    	
         return null;
     }
 
+    // Verifie s'il y a un cube de taille taille et de couleur couleur
     private Cube contientCube(final String couleur, final Taille taille) {
-        // TODO Auto-generated return
+    	for (Cube c : cubes) {
+    		if (c.estMemeCouleur(couleur) && c.peutAccueillir(taille)) {
+    			return c;
+    		}
+    	}
         return null;
     }
 
+    // pose le cube sur la table
     public void poserSurTable(final Cube cube) {
+    	ajouterTetePile(cube);
     }
 
+    // Ajoute un cube en tête de pile
     private void ajouterTetePile(final Cube tete) {
+    	if (tete != null) {
+    		cubes.add(tete);
+    	}
     }
 
+    // Retire un cube en tête de pile
     private void retirerTetePile(final Cube tete) {
+    	cubes.remove(tete);
     }
 
+    // Pose un cube sur un cube
     public void poserCubeSurCube(final Cube cube, final String couleur, final Taille taille) {
+    	Cube c = contientCube(couleur, taille);
+    	if (c != null) {
+    		empilerCubeSurCube(cube,c);
+    	} else {
+    		System.out.println("Nous n'avons pas trouvé de cube disponible ...");
+    	}
     }
 
+    // Empile un cube sur un autre cube
     private void empilerCubeSurCube(final Cube ancienneTete, final Cube nouvelleTete) {
+    	nouvelleTete.setSuivant(ancienneTete);
+    	retirerTetePile(ancienneTete);
+    	ajouterTetePile(nouvelleTete);
     }
 
+    // Dépile un cube
     public Cube depiler(final Cube cube) {
-        // TODO Auto-generated return
-        return null;
+        retirerTetePile(cube);
+        
+        Cube dessous = cube.getSuivant();
+        if (dessous != null) {
+        	ajouterTetePile(dessous);
+        	cube.setSuivant(null);
+        }
+        return cube;
     }
 
 }
